d70c5f6325ea2aa20f7c15f30f80372a51d45f2a
dimb29 <dimasbagas321@gmail.com>
2022-05-14 11:30:01 +0700

    jobnew

---
+++ resources/views/livewire/posts/job.blade.php
@@ -0,0 +1,29 @@
+<div class="font-bold text-xl mb-2">{{ $post->title }}</div>
+                <div class="flex">
+                    by&nbsp;<span class="italic">{{ $post->author->first_name . ' ' . $post->author->last_name }}</span>
+                    &nbsp;in&nbsp;<a href="{{ url('dashboard/categories/' . $post->category->id . '/posts') }}"
+                        class="underline">{{ $post->category->title }}</a>
+                    &nbsp;on&nbsp;{{ $post->updated_at->format('F, d Y') }}
+                </div>
+                <div class="grid grid-flow-col">
+                    @foreach ($post->images as $image)
+                        <div class="px-6 py-4">
+                            <img src="{{ $image->url }}" alt="{{ $image->description }}" width="100%">
+                        </div>
+                    @endforeach
+                </div>
+                <div id="content" class="text-gray-700 text-base m-auto" readonly="readonly">
+                    <p>{!! $post->content !!}</p>
+                </div>
+                <div class="flex">
+                    @php
+                    $tags=$post->tags->pluck('id', 'title');
+                    @endphp
+                    @if (count($tags) > 0)
+                        Tags:
+                        @foreach ($tags as $key => $tag)
+                            <a href="{{ url('dashboard/tags/' . $tag . '/posts') }}"
+                                class="underline px-1">{{ $key }}</a>
+                        @endforeach
+                    @endif
+                </div>
