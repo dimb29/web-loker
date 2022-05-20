70de2ac1b2f6159ae94e1d60a114d9901ef5157c
dimb29 <dimasbagas321@gmail.com>
2022-05-20 15:23:09 +0700

    ceckout

--- resources/views/livewire/main.blade.php
+++ resources/views/livewire/main.blade.php
@@ -100,7 +100,7 @@
                                     @endforeach
                                     <tr>
                                         <td colspan="2">
-                                            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                            <button class="bg-transparent hover:bg-indigo-400 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-indigo-400 hover:border-transparent rounded">
                                                 Selengkapnya
                                             </button>
                                         </td>
