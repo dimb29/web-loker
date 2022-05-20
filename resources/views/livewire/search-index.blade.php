aa18eb6a59358a63043477ae3f301cb035852463
dimb29 <dimasbagas321@gmail.com>
2022-05-20 09:50:09 +0700

    jobnew4

--- resources/views/livewire/search-index.blade.php
+++ resources/views/livewire/search-index.blade.php
@@ -1,76 +1,5 @@
-<div class="max-w-md">
-    <div class="flex flex-row">
-        <div class="pr-4">
-            <input type="search" id="search-title" list="title-list" name="searchtitle"
-            class="shadow w-96 h-8">
-            <datalist id="title-list">
-                 @foreach ($postsearch as $post)
-                <option value="{{$post->title}}">{{$post->title}}</option>
-                @endforeach
-            </datalist>
-        </div>
-    </div>
-</div>
-
-
-        
-        <div class="pr-4">
-            <select id="searchjobcat" name="states[]"
-            class="select2-jobcat-multiple w-full" multiple
-            placeholder="Search news">
-                 @foreach ($postsearch as $post)
-                <option value="{{$post->title}}">{{$post->title}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="flex">
-            <select id="searchjobtype" name="states[]"
-            class="select2-jobtype-multiple w-full" multiple
-            placeholder="Search news">
-                 @foreach ($postsearch as $post)
-                <option value="{{$post->title}}">{{$post->title}}</option>
-                @endforeach
-            </select>
-        </div>
-</div>
-
-
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
-            @if (session()->has('message'))
-                <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
-                    role="alert">
-                    <div class="flex">
-                        <div>
-                            <p class="text-sm">{{ session('message') }}</p>
-                        </div>
-                    </div>
-                </div>
-            @endif
-
-
-
-
-
-
-
-        <div>
-    <input type="text" name="name" placeholder="Search..."
-        class="w-1/3 py-2 border-b-2 border-gray-600 outline-none focus:border-green-400">
-</div>
-<div>
-    <input type="text" name="name" placeholder="Search..."
-        class="w-1/3 py-2 border-b-2 border-green-600 outline-none focus:border-blue-400">
-</div>
-<div>
-    <input type="text" name="name" placeholder="Search..."
-        class="w-1/3 py-2 border-b-2 border-blue-600 outline-none focus:border-yellow-400">
-</div>
-</div>
-
-<br>
-
-<div class="w-full md:w-2/3 shadow p-5 rounded-lg bg-white">
+<div class="flex justify-center ...">
+<div class="w-full md:w-5/6 shadow-xl p-5 rounded-lg bg-white">
   <div class="relative">
 	<div class="absolute flex items-center ml-2 h-full">
 	  <svg class="w-4 h-4 fill-current text-primary-gray-dark" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -78,7 +7,13 @@
 	  </svg>
 	</div>
 
-	<input type="text" placeholder="Search by listing, location, bedroom number..." class="px-8 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
+	<input type="search" id="search-title" list="title-list" name="searchtitle" type="text" placeholder="Search by listing, location, bedroom number..." 
+	class="px-8 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
+	<datalist id="title-list">
+                 @foreach ($postsearch as $post)
+                <option value="{{$post->title}}">{{$post->title}}</option>
+                @endforeach
+            </datalist>
 	  </div>
 
 	<div class="flex items-center justify-between mt-4">
@@ -94,25 +29,25 @@
 	<div>
 	  <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
 		<select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
-		  <option value="">All Type</option>
-		  <option value="for-rent">For Rent</option>
-		  <option value="for-sale">For Sale</option>
+		  <option value="">Semua Lokasi</option>
+		  @foreach ($postsearch as $post)
+                <option value="{{$post->title}}">{{$post->title}}</option>
+                @endforeach
 		</select>
 
 		<select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
-		  <option value="">Furnish Type</option>
-		  <option value="fully-furnished">Fully Furnished</option>
-		  <option value="partially-furnished">Partially Furnished</option>
-		  <option value="not-furnished">Not Furnished</option>
+		  <option value="">Semua Lulusan</option>
+		  @foreach ($postsearch as $post)
+                <option value="{{$post->title}}">{{$post->title}}</option>
+                @endforeach
 		</select>
 
 		<select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
-		  <option value="">Any Price</option>
-		  <option value="1000">RM 1000</option>
-		  <option value="2000">RM 2000</option>
-		  <option value="3000">RM 3000</option>
-		  <option value="4000">RM 4000</option>
-		</select>
+		  <option value="">Semua Tipe Pekerjaan</option>
+		  <option value="1000">Fulltime</option>
+		  <option value="2000">Part Time</option>
+		  <option value="3000">Freelance</option>
+		  </select>
 
 		<select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
 		  <option value="">Floor Area</option>
@@ -123,34 +58,10 @@
 		  <option value="1000 sq.ft">1000</option>
 		  <option value="1200 sq.ft">1200</option>
 		</select>
-
-		<select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
-		  <option value="">Bedrooms</option>
-		  <option value="1">1 bedroom</option>
-		  <option value="2">2 bedrooms</option>
-		  <option value="3">3 bedrooms</option>
-		  <option value="4">4 bedrooms</option>
-		  <option value="5">5 bedrooms</option>
-		</select>
-
-		<select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
-		  <option value="">Bathrooms</option>
-		  <option value="1">1 bathroom</option>
-		  <option value="2">2 bathrooms</option>
-		  <option value="3">3 bathrooms</option>
-		  <option value="4">4 bathrooms</option>
-		  <option value="5">5 bathrooms</option>
-		</select>
-
-		<select class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
-		  <option value="">Bathrooms</option>
-		  <option value="1">1 space</option>
-		  <option value="2">2 space</option>
-		  <option value="3">3 space</option>
-		</select>
 	  </div>
 	</div>
   </div>
+</div>
 
 <script>
     $(document).ready(function(){
