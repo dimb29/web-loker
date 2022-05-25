879751af275624c26fd38a87773371579478d68d
dimb29 <dimasbagas321@gmail.com>
2022-05-25 16:24:41 +0700

    nui

--- resources/views/livewire/main.blade.php
+++ resources/views/livewire/main.blade.php
@@ -27,38 +27,7 @@
                     </div>
                 </div>
             @endif
-
-
-            <div class="p-6">
-                <div class="slider ">
-                    @foreach ($trend as $post)
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg hover:bg-gray-300 m-8
-                        rounded-lg hover:text-blue-600 transition duration-150 transform hover:scale-110 hover:-translate-y-2">
-                                <a wire:click="countview({{ $post->id}})" href="{{ url('dashboard/posts', $post->id) }}">
-                                    
-                                    <img class="object-cover h-48 w-96" src="{{ $post->url }}">
-                                    <div class="p-6">
-                                        <h5 class="text-gray-900 text-xl font-medium mb-2 h-16 pb-4">{{ $post->title }}</h5>
-                                        <p class="text-gray-500 text-base mb-4">
-                                        PT Istana Kemakmuran Motor
-                                        </p>
-                                    </div>
-                                    <div div="grid grid-cols-2 gap-4">
-                                        <div class="p-6 font-medium text-sm text-gray-400">
-                                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                                            SMA/SMK
-                                        </div>
-                                        <div class="p-6 font-medium text-sm text-gray-400">
-                                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                                            SMA/SMK
-                                        </div>
-                                    </div>
-                                </a>
-                            
-                        </div>
-                @endforeach
-                </div>
-            </div>
+            <livewire:slider>
 </div>
             
 
@@ -190,6 +159,24 @@
                 }
             }, 50);
         }());
+        var ApiWilIndo = "https://dev.farizdotid.com/api/daerahindonesia/"
+        $.get(ApiWilIndo + 'provinsi',  // url
+        function (data, textStatus, jqXHR) {  // success callback
+            console.log(data)
+            var i;
+            for(i=0; i < data.provinsi.length; i++){
+                // console.log(data.provinsi[i])
+                datprov = data.provinsi[i]
+                var apdata = '<option value="'+datprov.id+'">'+datprov.nama+'</option>'
+                // console.log(apdata)
+                $('#sel-loc').append(apdata)
+            }
+        });
+            window.onscroll = function (ev) {
+                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                    window.livewire.emit('post-data');
+                }
+            };
     });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
