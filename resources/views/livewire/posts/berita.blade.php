a6311b2c733e9d32bde4e67bc9bbfdb288323231
dimb29 <dimasbagas321@gmail.com>
2022-06-03 16:58:43 +0700

    berita push

--- resources/views/livewire/posts/berita.blade.php
+++ resources/views/livewire/posts/berita.blade.php
@@ -28,8 +28,8 @@
                                     <div class="flex flex-col">
                                         @if($posts != '')
                                             @foreach ($posts as $post)
-                                                <div wire:click="postDetail({{$post->id}})"
-                                                data-id="{{ $post->id}}" class="daft-job bg-white rounded-lg shadow-xl my-6 transition duration-150 transform hover:scale-110 hover:-translate-y-2 
+                                                <div wire:click="postDetail({{$post->post_id}})"
+                                                data-id="{{ $post->post_id}}" class="daft-job bg-white rounded-lg shadow-xl my-6 transition duration-150 transform hover:scale-110 hover:-translate-y-2 
                                                         text-grey-500 hover:text-blue-500 cursor-pointer transition border-b border-r" data-mdb-ripple="true" data-mdb-ripple-color="light">
                                                         <img class="object-cover h-48 w-screen rounded-lg"src="{{ $post->url }}">
                                                         <div class="p-6 -mt-4">
@@ -52,36 +52,29 @@
                                                             $years = $thistime->diffInYears($post->updated_at);
                                                             @endphp
                                                             @if($minutes <= 60)
-                                                                {{$minutes}} Menit yang lalu
+                                                                {{$minutes}} menit yang lalu
                                                             @elseif($hours <= 24)
-                                                                {{$hours}} Jam yang lalu
+                                                                {{$hours}} jam yang lalu
                                                             @elseif($days <= 7)
-                                                                {{$days}} Hari yang lalu
+                                                                {{$days}} hari yang lalu
                                                             @elseif($weeks <= 4)
-                                                                {{$weeks}} Minggu yang lalu
+                                                                {{$weeks}} minggu yang lalu
                                                             @elseif($months <= 12)
-                                                                {{$months}} Bulan yang lalu
+                                                                {{$months}} bulan yang lalu
                                                             @else
-                                                                {{$years}} Tahun yang lalu
+                                                                {{$years}} tahun yang lalu
                                                             @endif
                                                         </p>
                                                         </div>
                                                     </div>
                                             @endforeach
                                         @endif
-                                            <div>
-                                                <div colspan="2">
-                                                    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                                                        Selengkapnya
-                                                    </button>
-                                                </div>
-                                            </div>
                                     </div>
                                 </div>
                             </div>
                         </div> 
                         @if($post_detail != null)
-                        <div class="hidden space-x-8 sm:flex w-4/5">
+                        <div class="hidden space-x-8 sm:flex w-4/5 myframe">
                             <div class="child top-0">
                             <livewire:post-data :post="$post_detail" :key="$post_detail['id']"/>
                             </div>
@@ -108,17 +101,17 @@
 
 <script>
     $(document).ready(function(){
-        // $('.daft-job').click(function(){
-        //     var dataId = $(this).attr("data-id");
-        //     console.log(dataId);
-        //     var url = "{{url('dashboard/posts/')}}/"+dataId+" #post-frame";
-        //     console.log(url);
-        //     if($('.myframe').is(":visible")){
-        //         $('.myframe').load("{{url('dashboard/posts/')}}/"+dataId+" #post-frame");
-        //     } else{
-        //         window.open("{{url('dashboard/posts/')}}/"+dataId,'_blank');
-        //     }
-        // })
+        $('.daft-job').click(function(){
+            var dataId = $(this).attr("data-id");
+            console.log(dataId);
+            var url = "{{url('dashboard/posts/')}}/"+dataId+" #post-frame";
+            console.log(url);
+            if($('.myframe').is(":visible")){
+                $('.myframe').load("{{url('dashboard/posts/')}}/"+dataId+" #post-frame");
+            } else{
+                window.open("{{url('dashboard/posts/')}}/"+dataId,'_blank');
+            }
+        })
     if($('.myframe').is(":visible")){
         $('.slider').slick({
             arrows: true,
