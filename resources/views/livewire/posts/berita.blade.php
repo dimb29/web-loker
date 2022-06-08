c7b6112c51333431cc727c8b03ae1c8e2821644b
dimb29 <dimasbagas321@gmail.com>
2022-06-08 13:16:45 +0700

    add post perushaan, saveloker, navbar

--- resources/views/livewire/posts/berita.blade.php
+++ resources/views/livewire/posts/berita.blade.php
@@ -16,9 +16,6 @@
         </div>
     </div>
             
-
-
-      
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-8">
     <div class="flex flex-row">
         <div class="flex-auto m-2 space-x-8 w-96">
@@ -27,9 +24,11 @@
                     <div class="flex flex-col">
                         @if($posts != '')
                             @foreach ($posts as $post)
-                                <div wire:click="postDetail({{$post->post_id}})"
-                                data-id="{{ $post->post_id}}" class="daft-job bg-white rounded-lg shadow-xl my-6 transition duration-150 transform hover:scale-110 hover:-translate-y-2 
-                                    text-grey-500 hover:text-blue-500 cursor-pointer transition border-b border-r" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                <div wire:click="postDetail({{$post->post_id}})" id="daft_job"
+                                data-id="{{ $post->post_id}}" class="daft-job bg-white rounded-lg shadow-xl 
+                                    mt-4 transition duration-150 transform hover:scale-110 hover:-translate-y-2 
+                                    text-grey-500 hover:text-blue-500 cursor-pointer transition border-b border-r" 
+                                    data-mdb-ripple="true" data-mdb-ripple-color="light">
                                     <img class="object-cover h-48 w-screen rounded-lg"src="{{ $post->url }}">
                                     <div class="p-6 -mt-4">
                                         <h5 class="text-gray-900 text-xl font-semibold h-16  mb-8">
@@ -38,8 +37,13 @@
                                         <h5 class="text-gray-900 text-xl font-medium">
                                             {{ $post->author->first_name . ' ' . $post->author->last_name }}
                                         </h5>
+                                        <h5 class="text-gray-900 text-xl font-medium">
+                                        @foreach($post->perusahaan as $perusahaan)
+                                            {{$perusahaan->per_nama}}
+                                        @endforeach
+                                        </h5>
                                         <h5 class="text-gray-900 text-xl font-medium mb-8">
-                                            {{ $post->regency->name }}
+                                            {{ ucwords(strtolower($post->regency->name)) }}
                                         </h5>
                                         <p> 
                                             @php
@@ -66,6 +70,27 @@
                                         </p>
                                     </div>
                                 </div>
+                                @if(Auth::user() != null)
+                                @php $getpostid = null @endphp
+                                @foreach($simpan_job as $simjob)
+                                @if($simjob->post_id == $post->post_id)
+                                @php $getpostid = $simjob->post_id @endphp
+                                @endif
+                                @endforeach
+                                @if($getpostid == $post->post_id)
+                                    <div class="-mt-14 mb-14 text-right z-10 px-4">
+                                        <button wire:click="delSaveJob({{$post->post_id}})" class="w-10 h-10 focus:outline-none rounded-3xl hover:bg-gray-300">
+                                            <i class="fa-solid fa-bookmark"></i>
+                                        </button>
+                                    </div>
+                                @else
+                                    <div class="-mt-14 mb-14 text-right z-10 px-4">
+                                        <button wire:click="saveJob({{$post->post_id}})" class="w-10 h-10 focus:outline-none rounded-3xl hover:bg-gray-300">
+                                            <i class="fa-regular fa-bookmark"></i>
+                                        </button>
+                                    </div>
+                                @endif
+                                @endif
                             @endforeach
                         @endif
                     </div>
@@ -84,12 +109,13 @@
                     <img class="mx-72 mt-48 h-48 w-48" src="https://th.jobsdb.com/assets/2982a5e7e83c56a68c79.png">
                 </div>
                 <div class="w-full sm:m-10">
-                <h5 class="font-bold font-serif text-xl text-center mr-12">
-                    We have 30,226 jobs for you
-                </h5>
-                <h5 class="font-medium font-serif text-xl text-center mr-12">
-                    Select a job to view details
-                </h5>
+                    <h5 class="font-bold font-serif text-xl text-center mr-12">
+                        We have 30,226 jobs for you
+                    </h5>
+                    <h5 class="font-medium font-serif text-xl text-center mr-12">
+                        Select a job to view details
+                    </h5>
+                </div>
             </div>
         @endif
     </div>
@@ -103,7 +129,7 @@
             var url = "{{url('dashboard/posts/')}}/"+dataId+" #post-frame";
             console.log(url);
             if($('.myframe').is(":visible")){
-                $('.myframe').load("{{url('dashboard/posts/')}}/"+dataId+" #post-frame");
+                // $('.myframe').load("{{url('dashboard/posts/')}}/"+dataId+" #post-frame");
             } else{
                 window.open("{{url('dashboard/posts/')}}/"+dataId,'_blank');
             }
@@ -119,6 +145,7 @@
             autoplaySpeed: 2000,
         });
     }else{
+        document.getElementById("daft_job").removeAttribute('wire:click')
         $('.slider').slick({
             arrows: true,
             slidesToShow: 1,
@@ -129,20 +156,6 @@
             autoplaySpeed: 2000,
         });
     }
-
-        (function(){
-            var width = screen.width,
-            height = screen.height;
-            
-            setInterval(function () {
-                if (screen.width !== width || screen.height !== height) {
-                    width = screen.width;
-                    height = screen.height;
-                    $(window).trigger('resolutionchange');
-                    alert("resolution change")
-                }
-            }, 50);
-        }());
         // var ApiWilIndo = "https://dev.farizdotid.com/api/daerahindonesia/"
         // $.get(ApiWilIndo + 'provinsi',  // url
         // function (data, textStatus, jqXHR) {  // success callback
